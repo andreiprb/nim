94747d0cead9b80caaf5265a7c8e0a00a3c9e4b4
andreiprb <andreipriboi@icloud.com>
2025-02-12 21:23:35 +0100

    added try catch for input

--- play.py
+++ play.py
@@ -18,11 +18,20 @@ if __name__ == '__main__':
 
         available_actions = NimLogic.available_actions(game.piles)
 
+
+        def get_int(prompt):
+            while True:
+                user_input = input(prompt)
+                try:
+                    return int(user_input)
+                except ValueError:
+                    continue
+
         if game.player == human_player:
             print("Your Turn")
             while True:
-                pile = int(input("Choose Pile: "))
-                count = int(input("Choose Count: "))
+                pile = get_int("Choose Pile: ")
+                count = get_int("Choose Count: ")
                 if (pile, count) in available_actions:
                     break
                 print("Invalid move, try again.")
